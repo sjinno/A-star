9962ad2f16fe1f33f58aed9eb138840c216f35e8
Shohei Jinno <shohei.jinno@gmail.com>
2021-04-12 09:34:38 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# A-star
+A* search algorithm ⭐
---
+++ astar.py
@@ -0,0 +1,7 @@
+import pygame
+import math
+from queue import PriorityQueue
+
+SIZE = 800
+WIN = pygame.display.set_mode((SIZE, SIZE))
+pygame.display.set_caption("A* Search Algorithm")
